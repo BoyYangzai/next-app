0d63d3ccab742822d15f5ffc4d21520c787863e5
BoyYangzai <2365539910@qq.com>
2025-07-29 18:06:56 +0800

    feat: 更新 Dockerfile 和 next.config.mjs，优化构建和国际化支持
    - 修改 Dockerfile，简化依赖安装和文件复制步骤，确保构建过程高效
    - 更新 next.config.mjs，添加动态加载翻译功能，支持多语言国际化
    - 修改 package.json 启动脚本，调整为使用独立服务器启动

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,34 @@
-/** @type {import('next').NextConfig} */
+import path from "path";
+import fs from "fs";
+
+const getAllTranslations = () => {
+  const localesDir = path.join(process.cwd(), "/i18n/locales");
+  const translations = {};
+
+  try {
+    fs.readdirSync(localesDir).forEach((lang) => {
+      translations[lang] = {};
+      const langPath = path.join(localesDir, lang);
+      fs.readdirSync(langPath).forEach((file) => {
+        if (file.endsWith(".json")) {
+          const namespace = path.basename(file, ".json");
+          translations[lang][namespace] = JSON.parse(
+            fs.readFileSync(path.join(langPath, file), "utf-8"),
+          );
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Error loading translations:", error);
+    return {};
+  }
+  return translations;
+};
+
+const translations = getAllTranslations();
+
 const nextConfig = {
+  output: "standalone",
   async redirects() {
     return [
       // Basic redirect
@@ -9,6 +38,9 @@ const nextConfig = {
         permanent: true,
       },
     ];
+  },
+  env: {
+    NEXT_PUBLIC_TRANSLATIONS: JSON.stringify(translations),
   },
 };
 
